bc7caaf61a3e7535ef3095d80a0bde9b8b849cc7
Phionx <shanjha.usa@gmail.com>
2019-04-04 00:47:01 -0400

    spin correlation stuff done

--- Lab 2/Analysis/chisquareSC.py
+++ Lab 2/Analysis/chisquareSC.py
@@ -111,9 +111,8 @@ def nu_func(x, *parameters):
 #R(x) = |x|^{-\eta}
 #Theory  Value: 1/4
 def eta_func(x, *parameters):
-	T_c   = parameters[0]
-	eta   = parameters[1]
-	constant = parameters[2]
+	eta   = parameters[0]
+	constant = parameters[1]
 
 	y = constant*np.abs(x)**(-1.0*eta)
 	return y
@@ -238,28 +237,6 @@ for iteration in range(num_temps):
 	yerror_data               = SC_std[iteration]
 	fig_num                   = 1
 
-
-
-# # parameter_estimates[0]    = 2.3
-# fit_center_index          = find_nearest(xdata_data, parameter_estimates[0])
-
-# fit_fraction_left_outer   = 1.0
-# fit_fraction_left_inner   = 1.0
-# fit_fraction_right_outer  = 1.0
-# fit_fraction_right_inner  = 1.0
-
-# data_points               = len(xdata_data)
-# xdata_fit_left            = xdata_data[fit_center_index - int(fit_fraction_left_outer*fit_center_index):fit_center_index - int(fit_fraction_left_inner*fit_center_index)]
-# xdata_fit_right           = xdata_data[fit_center_index + int(fit_fraction_right_inner*(data_points-fit_center_index-1)):fit_center_index + int(fit_fraction_right_outer*(data_points-fit_center_index))]
-# xdata_fit                 = np.concatenate((xdata_fit_left, xdata_fit_right), axis=None)
-# ydata_fit_left            = ydata_data[fit_center_index - int(fit_fraction_left_outer*fit_center_index):fit_center_index - int(fit_fraction_left_inner*fit_center_index)]
-# ydata_fit_right           = ydata_data[fit_center_index + int(fit_fraction_right_inner*(data_points-fit_center_index-1)):fit_center_index + int(fit_fraction_right_outer*(data_points-fit_center_index))]
-# ydata_fit                 = np.concatenate((ydata_fit_left, ydata_fit_right), axis=None)
-
-# yerror_fit_left           = yerror_data[fit_center_index - int(fit_fraction_left_outer*fit_center_index):fit_center_index - int(fit_fraction_left_inner*fit_center_index)]
-# yerror_fit_right          = yerror_data[fit_center_index + int(fit_fraction_right_inner*(data_points-fit_center_index-1)):fit_center_index + int(fit_fraction_right_outer*(data_points-fit_center_index))]
-# yerror_fit                = np.concatenate((yerror_fit_left, yerror_fit_right), axis=None)
-
 	xdata_fit  = xdata_data
 	ydata_fit  = ydata_data
 	yerror_fit = yerror_data
@@ -298,6 +275,8 @@ for iteration in range(num_temps):
 	# plot_residuals(xdata_fit, ydata_fit, yerror_fit, xfit, yfit, fig_num, title=title, x_label=x_label, y_label=y_label)
 	print("Spin Correlation Fit: END\n")
 	# plt.show()
+XI = np.array(XI)
+XI_err = np.array(XI_err)
 #-----------------------------------------------------------------------------------------------------------------------------------------
 
 
@@ -311,8 +290,8 @@ Estimate_nu               = 1.0
 scale_constant            = 1.0
 
 parameter_estimates       = [Estimate_T_c_nu, Estimate_nu, scale_constant]
+# var                       = [[-10, -10, -100],[10, 10, 100]]
 var                       = [[-10, -10, -100],[10, 10, 100]]
-
 # parameter_estimates       = [Estimate_xi, scale_constant]
 # var                       = [[-10000, -10000],[10000, 10000]]
 
@@ -322,7 +301,7 @@ function                  = nu_func
 xdata_data                = T_SC
 ydata_data                = XI
 yerror_data               = XI_err
-fig_num                   += 1
+fig_num                   = 1
 
 # plt.plot(xdata_data, ydata_data)
 # plt.show()
@@ -377,6 +356,190 @@ x_label  = "Temperature ($J/k_B$)"
 fit_eq   = ""
 plot_residuals(xdata_fit, ydata_fit, yerror_fit, xfit, yfit, fig_num, title=title, x_label=x_label, y_label=y_label)
 print("Spin Correlation Length Fit: END\n")
+
+#-----------------------------------------------------------------------------------------------------------------------------------------
+
+
+# Generate Data for eta fit
+#-----------------------------------------------------------------------------------------------------------------------------------------
+# Optimal_T_c_nu     = Optimal_params[0]
+# Optimal_T_c_nu_err = Optimal_params_err[0] 
+
+XI_max     = XI[XI.argmax()]
+XI_max_err = XI_err[XI.argmax()] 
+
+
+print("XI_max : " + str(XI_max))
+print("XI_max_err : " + str(XI_max_err))
+average_center            = XI.argmax()
+average_left              = average_center - 10
+average_right             = average_center + 10
+
+# print("average : " + str(average_left) + ", " + str(average_center) + ", " + str(average_right))
+SC_mean_centered = np.transpose(np.array(SC_mean[average_left:average_right+1]))
+
+SC_mean_avg = []
+SC_mean_std = []
+
+for row in SC_mean_centered:
+	SC_mean_avg.append(np.mean(row))
+	SC_mean_std.append(np.std(row))
+
+SC_mean_avg = np.array(SC_mean_avg)
+SC_mean_std = np.array(SC_mean_std)
+
+print(SC_mean_avg)
+print(SC_mean_std)
+#-----------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+# Find exponential fit near T_c
+#-----------------------------------------------------------------------------------------------------------------------------------------
+iteration                 = XI.argmax()
+
+print("Spin Correlation Exp Fit near T_c" + str(T_SC[iteration]) + " : BEGIN\n")
+
+T = T_SC[iteration]
+print("T_c : " + str(T))
+Estimate_xi               = 1.0
+scale_constant            = 1.0
+# add_constant              = 1.0
+
+# parameter_estimates       = [Estimate_xi, scale_constant, add_constant]
+# var                       = [[-10000, -10000, -100000],[10000, 10000, 100000]]
+
+parameter_estimates       = [Estimate_xi, scale_constant]
+var                       = [[-10000, -10000],[10000, 10000]]
+
+
+parameter_bound_ranges    = ([parameter_estimates[i] + var[0][i] for i in range(len(var[0]))], [parameter_estimates[i] + var[1][i] for i in range(len(var[1]))])
+function                  = xi_func
+xdata_data                = X[iteration]
+ydata_data                = SC_mean[iteration]
+yerror_data               = SC_std[iteration]
+fig_num                   += 1
+
+xdata_fit  = xdata_data
+ydata_fit  = ydata_data
+yerror_fit = yerror_data
+
+popt, perr            = optimal_fit(function, xdata_fit, ydata_fit, yerror_fit, parameter_estimates, parameter_bound_ranges)
+Optimal_params        = [x for x in popt]
+Optimal_params_err    = [x for x in perr]
+
+xi_at_T_c     = Optimal_params[0]
+xi_at_T_c_err = Optimal_params_err[0]
+
+# Optimal_params_names  = ["$\\xi$", "Constant A", "Constant B"]
+Optimal_params_names  = ["$\\xi$", "Constant A"]
+
+chi_square_min        = chi_square(function, xdata_fit, ydata_fit, yerror_fit, *Optimal_params)
+Optimal_params.append(chi_square_min)
+Optimal_params_err.append("")
+Optimal_params_names.append("Min Chi Square")
+
+
+xfit = xdata_fit
+yfit = function(xfit, *Optimal_params)
+
+title    = "Spin Correlation near $T_c$ vs. Distance (Exponential Fit)"
+y_label  = "Spin Correlation"
+x_label  = "Distance (#)"
+fit_eq   = "$R(x) = A*e^{-x/\\xi}$"
+
+plot_fit(xdata_data, ydata_data, yerror_data, xfit, yfit, Optimal_params, Optimal_params_err, Optimal_params_names, fig_num, title=title, x_label=x_label, y_label=y_label, fit_eq=fit_eq)
+
+fig_num += 1
+title    = "Spin Correlation near $T_c$ vs. Distance (Exponential Fit: Residuals)"
+y_label  = "Spin Correlation"
+x_label  = "Distance (#)"
+fit_eq   = ""
+plot_residuals(xdata_fit, ydata_fit, yerror_fit, xfit, yfit, fig_num, title=title, x_label=x_label, y_label=y_label)
+print("Spin Correlation Exp Fit near T_c: END\n")
+# plt.show()
+#-----------------------------------------------------------------------------------------------------------------------------------------	
+
+
+# Spin Correlation near T_c: \eta
+#-----------------------------------------------------------------------------------------------------------------------------------------
+
+print("Spin Correlation Near T_c: BEGIN\n")
+Estimate_eta               = 1.0
+scale_constant            = 1.0
+
+parameter_estimates       = [Estimate_eta, scale_constant]
+var                       = [[-10, -10],[10, 10]]
+
+# parameter_estimates       = [Estimate_xi, scale_constant]
+# var                       = [[-10000, -10000],[10000, 10000]]
+
+
+parameter_bound_ranges    = ([parameter_estimates[i] + var[0][i] for i in range(len(var[0]))], [parameter_estimates[i] + var[1][i] for i in range(len(var[1]))])
+function                  = eta_func
+xdata_data                = X[0]
+ydata_data                = SC_mean_avg
+yerror_data               = SC_mean_std
+fig_num                   += 1
+
+# plt.plot(xdata_data, ydata_data)
+# plt.show()
+
+# parameter_estimates[0]    = 2.3
+# fit_center_index          = find_nearest(xdata_data, XI_max)
+fit_center_index          = find_nearest(xdata_data, XI_max)
+
+fit_fraction_left_outer   = 1.0
+fit_fraction_left_inner   = 1.0/20
+fit_fraction_right_outer  = 0.0
+fit_fraction_right_inner  = 0.0
+
+data_points               = len(xdata_data)
+xdata_fit_left            = xdata_data[fit_center_index - int(fit_fraction_left_outer*fit_center_index):fit_center_index - int(fit_fraction_left_inner*fit_center_index)]
+xdata_fit_right           = xdata_data[fit_center_index + int(fit_fraction_right_inner*(data_points-fit_center_index-1)):fit_center_index + int(fit_fraction_right_outer*(data_points-fit_center_index))]
+xdata_fit                 = np.concatenate((xdata_fit_left, xdata_fit_right), axis=None)
+ydata_fit_left            = ydata_data[fit_center_index - int(fit_fraction_left_outer*fit_center_index):fit_center_index - int(fit_fraction_left_inner*fit_center_index)]
+ydata_fit_right           = ydata_data[fit_center_index + int(fit_fraction_right_inner*(data_points-fit_center_index-1)):fit_center_index + int(fit_fraction_right_outer*(data_points-fit_center_index))]
+ydata_fit                 = np.concatenate((ydata_fit_left, ydata_fit_right), axis=None)
+
+yerror_fit_left           = yerror_data[fit_center_index - int(fit_fraction_left_outer*fit_center_index):fit_center_index - int(fit_fraction_left_inner*fit_center_index)]
+yerror_fit_right          = yerror_data[fit_center_index + int(fit_fraction_right_inner*(data_points-fit_center_index-1)):fit_center_index + int(fit_fraction_right_outer*(data_points-fit_center_index))]
+yerror_fit                = np.concatenate((yerror_fit_left, yerror_fit_right), axis=None)
+
+print(str(xdata_fit))
+print(str(ydata_fit))
+
+popt, perr            = optimal_fit(function, xdata_fit, ydata_fit, yerror_fit, parameter_estimates, parameter_bound_ranges)
+Optimal_params        = [x for x in popt]
+Optimal_params_err    = [x for x in perr]
+Optimal_params_names  = ["$\\eta$", "Constant C"]
+# Optimal_params_names  = ["$\\xi$", "Constant A"]
+
+chi_square_min        = chi_square(function, xdata_fit, ydata_fit, yerror_fit, *Optimal_params)
+Optimal_params.append(chi_square_min)
+Optimal_params_err.append("")
+Optimal_params_names.append("Min Chi Square")
+
+
+xfit = xdata_fit
+yfit = function(xfit, *Optimal_params)
+
+title    = "Spin Correlation Near $T_C$ vs. Distance (Power Law Fit)"
+y_label  = "Spin Correlation"
+x_label  = "Distance (#)"
+fit_eq   = "$R(x) = C*|x|^{-\\eta}$"
+
+plot_fit(xdata_data, ydata_data, yerror_data, xfit, yfit, Optimal_params, Optimal_params_err, Optimal_params_names, fig_num, title=title, x_label=x_label, y_label=y_label, fit_eq=fit_eq)
+
+fig_num += 1
+title    = "Spin Correlation Near $T_C$ vs. Distance (Power Law Fit: Residuals)"
+y_label  = "Spin Correlation"
+x_label  = "Distance (#)"
+fit_eq   = ""
+plot_residuals(xdata_fit, ydata_fit, yerror_fit, xfit, yfit, fig_num, title=title, x_label=x_label, y_label=y_label)
+print("Spin Correlation Near T_c: END\n")
 plt.show()
 #-----------------------------------------------------------------------------------------------------------------------------------------
+
+
 
